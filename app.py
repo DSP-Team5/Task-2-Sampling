8a4c49fe747f71e54f8989a54eb783f9024d70f3
HananTawfik <102436066+HananTawfik@users.noreply.github.com>
2023-05-04 13:43:29 +0300

    commit

--- app.py
+++ app.py
@@ -1,27 +1,19 @@
-# Import the necessary modules
+from flask import Flask, render_template,request, jsonify
 import numpy as np
 from scipy.fft import fft
-from flask import Flask, render_template,request, jsonify
 
-# Create the Flask app
 app = Flask(__name__)
 
-# Define the route and the HTTP method this endpoint will listen to
-# In this case, we want to receive POST requests to the '/calculate-fft-max' endpoint
+@app.route('/')
+def Sampling_Studio():
+    return render_template('main.html')
+
 @app.route('/calculate-fft-max', methods=['POST'])
-
-# Define the function that will be called when the endpoint is reached
 def calculate_fft_max():
-
-    # Get the array from the request object
-    # The request object contains all the information about the request
-    # In this case, the array will be in the body of the request
     array = request.get_json()
-
-    # Calculate the FFT of the array
     fft = np.fft.fft(array)
 
-    # Get the magnitudes of the FFT coefficients
+# Get the magnitudes of the FFT coefficients
     fft_magnitudes = np.abs(fft)
 
     # Get the frequencies corresponding to the FFT coefficients
@@ -33,6 +25,7 @@ def calculate_fft_max():
 
     # Get the frequency corresponding to the maximum magnitude
     max_frequency = frequencies[max_magnitude_index]
-
-    # Return the maximum frequency in JSON format
     return jsonify({'fftMaxMagnitude': max_frequency})
+
+if __name__ == '__main__':
+    app.run(debug=True)
